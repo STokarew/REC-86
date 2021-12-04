5e9f88027b6a65eda634f634ab237a745950a929
сергей токарев <sseregatokarew@gmail.com>
2021-12-04 16:39:38 +0700

    Добавьте файлы проекта.

---
+++ REC-86/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace REC_86
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 6; i++)
+            {
+                for (int j = 2; j <= 6; j++)
+                {
+                    Console.Write($"{i}*{j}={i*j}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+    
+}
